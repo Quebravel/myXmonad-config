9a8006b5c76e672154b3fa435eb3325f2f122b51
Jonatas <jonatasgb@gmail.com>
2017-07-24 13:41:28 -0300

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,7 +8,6 @@ import XMonad.Layout.NoBorders
 import XMonad.Hooks.EwmhDesktops
 import System.IO
 
-myTerminal = "xfce4-terminal"
 myLayout = tiled ||| Mirror tiled ||| Full                      -- Faz a janela
   where                                                         -- em tela cheia ficar
     tiled   = Tall 1 (3/100) (3/5)                              -- sem borda
@@ -21,7 +20,6 @@ main = do
 	, borderWidth        = 1
 	, focusedBorderColor = "darkgray"
 	, normalBorderColor  = "black"
-	, terminal = myTerminal
 	, startupHook = setWMName "LG3D"
 	, layoutHook = avoidStruts $ smartBorders $ myLayout
 	, logHook = dynamicLogWithPP xmobarPP
@@ -29,18 +27,19 @@ main = do
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
         , handleEventHook    = fullscreenEventHook
-	, modMask = mod4Mask     -- Rebind Mod to the Windows key
+	, modMask = mod4Mask     -- Muda Mod para tecla Windows
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s") -- Win + Print = Atalho para tecla Print
+        [ 
+          ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s") -- Win + Print = Atalho para tecla Print
         , ((0, xK_Print), spawn "scrot") -- Telca Print Screen
-	, ((mod4Mask, xK_b), sendMessage ToggleStruts) -- Win + b = oculta o xmobar
-        
--- Volume Control
+	, ((mod4Mask, xK_b), sendMessage ToggleStruts) -- Win + b = Esconde o xmobar atras das janelas
+        , ((mod4Mask, xK_Return), spawn "xterm") -- Win + Enter = Segundo atalho para o terminal
+	
+-- Volume Controle
      , ((mod4Mask,               xK_F11   ), spawn "amixer set Master 5%-")
      , ((mod4Mask,               xK_F12   ), spawn "amixer set Master 5%+")
    
--- Brightness Control
+-- Brilho Controle
      , ((mod4Mask,               xK_F4    ), spawn "xbacklight -dec 10")
      , ((mod4Mask,               xK_F5    ), spawn "xbacklight -inc 10")
 	]
